b7e0e685112ece75c376ff960e62df56b50a86bf
Elijah Kotyluk <elijah@elijahkotyluk.com>
2020-04-05 18:02:37 -0700

    chore(locale): add locale object

---
+++ static/locale/index.js
@@ -0,0 +1,468 @@
+export const locale = {
+  usa: {
+    name: 'United States',
+    code: 'US',
+    states: [
+      {
+        name: 'Alabama',
+        value: 'AL'
+      },
+      {
+        name: 'Alaska',
+        value: 'AK'
+      },
+      {
+        name: 'American Samoa',
+        value: 'AS'
+      },
+      {
+        name: 'Arizona',
+        value: 'AZ'
+      },
+      {
+        name: 'Arkansas',
+        value: 'AR'
+      },
+      {
+        name: 'California',
+        value: 'CA'
+      },
+      {
+        name: 'Colorado',
+        value: 'CO'
+      },
+      {
+        name: 'Connecticut',
+        value: 'CT'
+      },
+      {
+        name: 'Delaware',
+        value: 'DE'
+      },
+      {
+        name: 'District Of Columbia',
+        value: 'DC'
+      },
+      {
+        name: 'Federated States Of Micronesia',
+        value: 'FM'
+      },
+      {
+        name: 'Florida',
+        value: 'FL'
+      },
+      {
+        name: 'Georgia',
+        value: 'GA'
+      },
+      {
+        name: 'Guam',
+        value: 'GU'
+      },
+      {
+        name: 'Hawaii',
+        value: 'HI'
+      },
+      {
+        name: 'Idaho',
+        value: 'ID'
+      },
+      {
+        name: 'Illinois',
+        value: 'IL'
+      },
+      {
+        name: 'Indiana',
+        value: 'IN'
+      },
+      {
+        name: 'Iowa',
+        value: 'IA'
+      },
+      {
+        name: 'Kansas',
+        value: 'KS'
+      },
+      {
+        name: 'Kentucky',
+        value: 'KY'
+      },
+      {
+        name: 'Louisiana',
+        value: 'LA'
+      },
+      {
+        name: 'Maine',
+        value: 'ME'
+      },
+      {
+        name: 'Marshall Islands',
+        value: 'MH'
+      },
+      {
+        name: 'Maryland',
+        value: 'MD'
+      },
+      {
+        name: 'Massachusetts',
+        value: 'MA'
+      },
+      {
+        name: 'Michigan',
+        value: 'MI'
+      },
+      {
+        name: 'Minnesota',
+        value: 'MN'
+      },
+      {
+        name: 'Mississippi',
+        value: 'MS'
+      },
+      {
+        name: 'Missouri',
+        value: 'MO'
+      },
+      {
+        name: 'Montana',
+        value: 'MT'
+      },
+      {
+        name: 'Nebraska',
+        value: 'NE'
+      },
+      {
+        name: 'Nevada',
+        value: 'NV'
+      },
+      {
+        name: 'New Hampshire',
+        value: 'NH'
+      },
+      {
+        name: 'New Jersey',
+        value: 'NJ'
+      },
+      {
+        name: 'New Mexico',
+        value: 'NM'
+      },
+      {
+        name: 'New York',
+        value: 'NY'
+      },
+      {
+        name: 'North Carolina',
+        value: 'NC'
+      },
+      {
+        name: 'North Dakota',
+        value: 'ND'
+      },
+      {
+        name: 'Northern Mariana Islands',
+        value: 'MP'
+      },
+      {
+        name: 'Ohio',
+        value: 'OH'
+      },
+      {
+        name: 'Oklahoma',
+        value: 'OK'
+      },
+      {
+        name: 'Oregon',
+        text: 'Oregon',
+        value: 'OR'
+      },
+      {
+
+        name: 'Palau',
+        text: 'Palau',
+        value: 'PW'
+      },
+      {
+
+        name: 'Pennsylvania',
+        text: 'Pennsylvania',
+        value: 'PA'
+      },
+      {
+
+        name: 'Puerto Rico',
+        text: 'Puerto Rico',
+        value: 'PR'
+      },
+      {
+
+        name: 'Rhode Island',
+        text: 'Rhode Island',
+        value: 'RI'
+      },
+      {
+
+        name: 'South Carolina',
+        text: 'South Carolina',
+        value: 'SC'
+      },
+      {
+
+        name: 'South Dakota',
+        text: 'South Dakota',
+        value: 'SD'
+      },
+      {
+
+        name: 'Tennessee',
+        text: 'Tennessee',
+        value: 'TN'
+      },
+      {
+
+        name: 'Texas',
+        text: 'Texas',
+        value: 'TX'
+      },
+      {
+
+        name: 'Utah',
+        text: 'Utah',
+        value: 'UT'
+      },
+      {
+
+        name: 'Vermont',
+        text: 'Vermont',
+        value: 'VT'
+      },
+      {
+
+        name: 'Virgin Islands',
+        text: 'Virgin Islands',
+        value: 'VI'
+      },
+      {
+
+        name: 'Virginia',
+        text: 'Virginia',
+        value: 'VA'
+      },
+      {
+
+        name: 'Washington',
+        text: 'Washington',
+        value: 'WA'
+      },
+      {
+
+        name: 'West Virginia',
+        text: 'West Virginia',
+        value: 'WV'
+      },
+      {
+
+        name: 'Wisconsin',
+        text: 'Wisconsin',
+        value: 'WI'
+      },
+      {
+
+        name: 'Wyoming',
+        text: 'Wyoming',
+        value: 'WY'
+      }
+    ]
+  },
+  canada: {
+    name: 'Canada',
+    code: 'CA',
+    states: [
+      {
+        name: 'Alberta',
+        code: 'AB'
+      },
+      {
+        name: 'British Columbia',
+        code: 'BC'
+      },
+      {
+        name: 'Manitoba',
+        code: 'MB'
+      },
+      {
+        name: 'New Brunswick',
+        code: 'NB'
+      },
+      {
+        name: 'Newfoundland and Labrador',
+        code: 'NL'
+      },
+      {
+        name: 'Nova Scotia',
+        code: 'NS'
+      },
+      {
+        name: 'Ontario',
+        code: 'ON'
+      },
+      {
+        name: 'Prince Edward Island',
+        code: 'PE'
+      },
+      {
+        name: 'Quebec',
+        code: 'QC'
+      },
+      {
+        name: 'Saskatchewan',
+        code: 'SK'
+      },
+      {
+        name: 'Northwest Territories',
+        code: 'NT'
+      },
+      {
+        name: 'Nunavut',
+        code: 'NU'
+      },
+      {
+        name: 'Yukon',
+        code: 'YT'
+      }]
+  },
+  mexico: {
+    name: 'Mexico',
+    code: 'MX',
+    states: [
+      {
+        state: 'Distrito Federal',
+        code: 'MX-DIF',
+      },
+      {
+        state: 'Aguascalientes',
+        code: 'MX-AGU',
+      },
+      {
+        state: 'Baja California',
+        code: 'MX-BCN',
+      },
+      {
+        state: 'Baja California Sur',
+        code: 'MX-BCS',
+      },
+      {
+        state: 'Campeche',
+        code: 'MX-CAM',
+      },
+      {
+        state: 'Chiapas',
+        code: 'MX-CHP',
+      },
+      {
+        state: 'Chihuahua',
+        code: 'MX-CHH',
+      },
+      {
+        state: 'Coahuila',
+        code: 'MX-COA',
+      },
+      {
+        state: 'Colima',
+        code: 'MX-COL',
+      },
+      {
+        state: 'Durango',
+        code: 'MX-DUR',
+      },
+      {
+        state: 'Guanajuato',
+        code: 'MX-GUA',
+      },
+      {
+        state: 'Guerrero',
+        code: 'MX-GRO',
+      },
+      {
+        state: 'Hidalgo',
+        code: 'MX-HID',
+      },
+      {
+        state: 'Jalisco',
+        code: 'MX-JAL',
+      },
+      {
+        state: 'Michoacán',
+        code: 'MX-MIC',
+      },
+      {
+        state: 'Morelos',
+        code: 'MX-MOR',
+      },
+      {
+        state: 'México',
+        code: 'MX-MEX',
+      },
+      {
+        state: 'Nayarit',
+        code: 'MX-NAY',
+      },
+      {
+        state: 'Nuevo León',
+        code: 'MX-NLE',
+      },
+      {
+        state: 'Oaxaca',
+        code: 'MX-OAX',
+      },
+      {
+        state: 'Puebla',
+        code: 'MX-PUE',
+      },
+      {
+        state: 'Querétaro',
+        code: 'MX-QUE',
+      },
+      {
+        state: 'Quintana Roo',
+        code: 'MX-ROO',
+      },
+      {
+        state: 'San Luis Potosí',
+        code: 'MX-SLP',
+      },
+      {
+        state: 'Sinaloa',
+        code: 'MX-SIN',
+      },
+      {
+        state: 'Sonora',
+        code: 'MX-SON',
+      },
+      {
+        state: 'Tabasco',
+        code: 'MX-TAB',
+      },
+      {
+        state: 'Tamaulipas',
+        code: 'MX-TAM',
+      },
+      {
+        state: 'Tlaxcala',
+        code: 'MX-TLA',
+      },
+      {
+        state: 'Veracruz',
+        code: 'MX-VER',
+      },
+      {
+        state: 'Yucatán',
+        code: 'MX-YUC',
+      },
+      {
+        state: 'Zacatecas',
+        code: 'MX-ZAC',
+      }
+    ]
+  }
+}
