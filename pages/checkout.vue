8b528628fa2f2936731a4d2cfa203e1c18ba47b4
Elijah Kotyluk <elijah@elijahkotyluk.com>
2020-04-10 22:07:57 -0700

    chore(checkout): update component
    Update template. Add listener to billing details component. add total computed prop that returns the
    total cost of the order + shipping. Add updateCost method to update cost property once emitted from
    billingdetails

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -5,7 +5,7 @@
         <v-card-title primary-title>
           Billing Details
         </v-card-title>
-        <billing-details />
+        <billing-details :cart="cart" @shippingCost="updateCost" />
       </v-col>
       <v-col cols="12" sm="5">
         <v-card max-width="400">
@@ -22,9 +22,9 @@
               :key="item.id"
               justify="space-between"
             >
-              <v-card-subtitle :key="item.name">{{
-                item.name
-              }}</v-card-subtitle>
+              <v-card-subtitle :key="item.name"
+                >{{ item.name }} x {{ item.quantity }}</v-card-subtitle
+              >
               <v-card-subtitle :key="item.price.formatted"
                 >${{ item.price.formatted }}</v-card-subtitle
               >
@@ -32,20 +32,21 @@
             <v-divider></v-divider>
             <v-row justify="space-between">
               <v-card-subtitle>Subtotal</v-card-subtitle>
-              <v-card-subtitle>$0.00</v-card-subtitle>
+              <v-card-subtitle>{{
+                cart.subtotal.formatted_with_symbol || '$0.00'
+              }}</v-card-subtitle>
             </v-row>
             <v-row justify="space-between">
               <v-card-subtitle>Shipping</v-card-subtitle>
-              <v-card-subtitle>$0.00</v-card-subtitle>
-            </v-row>
-            <v-row justify="space-between">
-              <v-card-subtitle>Tax</v-card-subtitle>
-              <v-card-subtitle>$0.00</v-card-subtitle>
+              <v-card-subtitle>${{ shippingCost }}</v-card-subtitle>
             </v-row>
             <v-divider></v-divider>
             <v-row justify="space-between">
               <v-card-subtitle>Total</v-card-subtitle>
-              <v-card-subtitle>$0.00</v-card-subtitle>
+              <v-card-subtitle>{{ total }}</v-card-subtitle>
+            </v-row>
+            <v-row justify="center">
+              <v-btn class="my-2" to="/">Edit Cart</v-btn>
             </v-row>
           </v-container>
         </v-card>
@@ -62,10 +63,23 @@ export default {
   components: {
     BillingDetails
   },
+  data: () => ({
+    shippingCost: '0.00'
+  }),
   computed: {
     ...mapGetters({
       cart: 'cart'
-    })
+    }),
+    total() {
+      const total = +this.cart.subtotal.raw + +this.shippingCost
+
+      return `$${total}`
+    }
+  },
+  methods: {
+    updateCost(price) {
+      this.shippingCost = price
+    }
   }
 }
 </script>
