75172e101ce6dea337f42ffdb022698dd5ea597f
Elijah Kotyluk <elijah@elijahkotyluk.com>
2020-04-12 23:45:04 -0700

    chore(layout): move nav drawer activator to the layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,7 +7,23 @@
             Demo Merchant
           </a>
         </v-toolbar-title>
+        <v-spacer></v-spacer>
+
+        <v-btn
+          class="drawer-btn"
+          color="black"
+          elevation="0"
+          outlined
+          right
+          @click.stop="drawer = !drawer"
+        >
+          <v-icon class="mr-4">mdi-cart-outline</v-icon>
+          <p v-if="cart.line_items" class="mb-1">
+            {{ cart.line_items.length }}
+          </p>
+        </v-btn>
       </v-toolbar>
+      <checkout v-model="drawer" :cart="cart" @update="drawer = !drawer" />
       <v-container>
         <nuxt />
       </v-container>
@@ -16,7 +32,22 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import Checkout from '~/components/Checkout.vue'
+
 export default {
+  components: {
+    Checkout
+  },
+  data: () => ({
+    drawer: false
+  }),
+  computed: {
+    ...mapGetters({
+      cart: 'cart',
+      subtotal: 'cartSubtotal'
+    })
+  },
   mounted() {
     this.$store.dispatch('retrieveCart')
   }
